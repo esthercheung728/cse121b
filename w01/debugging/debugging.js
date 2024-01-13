f7b4593f7e1b82bf61f2799b783ed40421cc5c69
esthercheung728 <esther_MayMay@yahoo.com.hk>
2024-01-13 21:49:44 +0800

    first commit

---
+++ w01/debugging/debugging.js
@@ -0,0 +1,23 @@
+const PI = 3.14;
+let radius = 3;
+let area = 0;
+area = radius * radius * PI;
+console.log("Area1:", area);
+radius = 4;
+area = radius * radius * PI;
+console.log("Area2:", area);
+
+
+function circleArea(radius) {
+    // code to complete our task here
+    const PI = 3.14;
+    let area = 0;
+    function circleArea(radius) {
+        const area = radius * rradius * PI;
+        return area;
+    }
+    area = circleArea(3);
+    console.log("Area1", area);
+    area = circleArea(4);
+    console.log("Area2", area);
+}
