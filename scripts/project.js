abc65a8200f476d36311c9010c6b3e8a5a0fbe09
esthercheung728 <esther_MayMay@yahoo.com.hk>
2024-02-17 02:57:08 +0800

    amend json

--- scripts/project.js
+++ scripts/project.js
@@ -1,54 +1,37 @@
-// // get parent element
-// const sectionCenter = document.querySelector(".section-center");
-// const btnContainer = document.querySelector(".btn-container");
+/* W06: Programming Tasks */
+
+/* Declare and initialize global variables */
 
 // Declare const variable and reference the HTML div 'menu'
-const menuElement = document.getElementById('menu');
+const menuElement = document.getElementById('restaurantMenu');
 let menuList = [];
 
 /* async displayMenu Function */
 
-const displayMenu = (menu) => {
-    menu.forEach(menu => {
+const displayMenu = (restaurantMenu) => {
+    restaurantMenu.forEach(menus => {
         const article = document.createElement('article');
 
         const h3 = document.createElement('h3');
-        h3.textContent = temple.templeName;
+        h3.textContent = menus.title;
 
         const img = document.createElement('img');
-        img.src = temple.imageUrl;
-        img.alt = temple.location;
+        img.src = menus.imageUrl;
+        img.alt = menus.category;
 
         article.appendChild(h3);
         article.appendChild(img);
 
-        templesElement.appendChild(article);
+        menuElement.appendChild(article);
     });
 };
 
-
-
-
-// display all items when page loads
-window.addEventListener("DOMContentLoaded", function () {
-    fetch("https://github.com/esthercheung728/cse121b/blob/main/images/menu.json")
-        .then(function (response) {
-            return response.json();
-        })
-        .then(function (data) {
-            diplayMenuItems(data);
-            displayMenuButtons(data);
-        })
-        .catch(function (error) {
-            console.log("Error fetching menu data:", error);
-        });
-});
 
 /* async getMenu Function using fetch()*/
 
 const getMenu = async () => {
     try {
-        const response = await fetch("https://github.com/esthercheung728/cse121b/blob/main/images/menu.json");
+        const response = await fetch("https://esthercheung728.github.io/cse121b/images/menu.json");
         if (response.ok) { menuList = await response.json() }
         // menuList = await response.json();
         displayMenu(menuList);
@@ -61,86 +44,53 @@ const getMenu = async () => {
 
 
 /* reset Function */
-const reset = () => sectionCenter.replaceChildren();
+// const reset = () => {
+//     menuElement.innerHTML = '';
+// };
+
+const reset = () => menuElement.replaceChildren();
+
+/* filterMenu Function */
 
 
+const sortBy = () => {
+    reset();
+    const filter = document.getElementById("filtered");
 
+    switch (filter.value) {
+        case 'pasta':
+            displayMenu(menuList.filter(menu => menus.category.includes('Pasta')));
+            break;
+        case 'salad':
+            displayMenu(menuList.filter(menu => menus.category.includes('Salad')));
+            break;
+        case 'snack':
+            displayMenu(menuList.filter(menu => menus.category.includes('Snack')));
+            break;
+        case 'rice':
+            displayMenu(menuList.filter(menu => menus.category.includes('Rice')));
+            break;
+        case 'burger':
+            displayMenu(menuList.filter(menu => menus.category.includes('Burger')));
+            break;
+        case 'pizza':
+            displayMenu(menuList.filter(menu => menus.category.includes('Pizza')));
+            break;
+        case 'dessert':
+            displayMenu(menuList.filter(menu => menus.category.includes('Dessert')));
+        case 'drinks':
+            displayMenu(menuList.filter(menu => menus.category.includes('Drinks')));
+            break;
 
+        case 'all':
+        default:
+            displayMenu(menuList);
+            break;
+    }
+};
 
+getMenu();
 
+/* Event Listener */
 
-
-
-
-
-
-
-
-
-
-
-
-// Function to display menu items
-function diplayMenuItems(menuItems) {
-    let displayMenu = menuItems.map(function (item) {
-        return `<article class="menu-item">
-            <img src=${item.img} alt=${item.title} class="photo" />
-            <div class="item-info">
-              <header>
-                <h4>${item.title}</h4>
-                <h4 class="price">$${item.price}</h4>
-              </header>
-              <p class="item-text">
-                ${item.desc}
-              </p>
-            </div>
-          </article>`;
-    });
-
-    displayMenu = displayMenu.join("");
-    sectionCenter.innerHTML = displayMenu;
-}
-
-// Function to display menu filter buttons
-function displayMenuButtons() {
-    // Get unique categories from the menu array
-    const categories = menu.reduce(
-        function (values, item) {
-            if (!values.includes(item.category)) {
-                values.push(item.category);
-            }
-            return values;
-        },
-        ["all"]
-    );
-
-    // Generate HTML for filter buttons
-    const categoryBtns = categories
-        .map(function (category) {
-            return `<button type="button" class="filter-btn" data-id=${category}>
-            ${category}
-          </button>`;
-        })
-        .join("");
-
-    btnContainer.innerHTML = categoryBtns;
-    const filterBtns = btnContainer.querySelectorAll(".filter-btn");
-
-    filterBtns.forEach(function (btn) {
-        btn.addEventListener("click", function (e) {
-            const category = e.currentTarget.dataset.id;
-            const menuCategory = menu.filter(function (menuItem) {
-                if (menuItem.category === category) {
-                    return menuItem;
-                }
-            });
-
-            // Display menu items based on the selected category
-            if (category === "all") {
-                diplayMenuItems(menu);
-            } else {
-                diplayMenuItems(menuCategory);
-            }
-        });
-    });
-}
+const filteredElement = document.querySelector('#filtered').addEventListener('change', sortBy);
---
+++ scripts/project2.js
@@ -0,0 +1,178 @@
+// // get parent element
+// const sectionCenter = document.querySelector(".section-center");
+// const btnContainer = document.querySelector(".btn-container");
+
+// // display all items when page loads
+// window.addEventListener("DOMContentLoaded", function () {
+//     fetch("https://raw.githubusercontent.com/esthercheung728/cse121b/main/images/menu.json")
+//         .then(function (response) {
+//             return response.json();
+//         })
+//         .then(function (data) {
+//             displayMenuItems(data);
+//             displayMenuButtons(data);
+//         })
+//         .catch(function (error) {
+//             console.log("Error fetching menu data:", error);
+//         });
+// });
+
+// // Function to display menu items
+// function displayMenuItems(menuItems) {
+//     let displayMenu = menuItems.map(function (item) {
+//         return `<article class="menu-item">
+//             <img src=${item.img} alt=${item.title} class="photo" />
+//             <div class="item-info">
+//               <header>
+//                 <h4>${item.title}</h4>
+//                 <h4 class="price">$${item.price}</h4>
+//               </header>
+//               <p class="item-text">
+//                 ${item.desc}
+//               </p>
+//             </div>
+//           </article>`;
+//     });
+
+//     displayMenu = displayMenu.join("");
+//     sectionCenter.innerHTML = displayMenu;
+// }
+
+// // Function to display menu filter buttons
+// function displayMenuButtons() {
+//     // Get unique categories from the menu array
+//     const categories = menu.reduce(
+//         function (values, item) {
+//             if (!values.includes(item.category)) {
+//                 values.push(item.category);
+//             }
+//             return values;
+//         },
+//         ["all"]
+//     );
+
+//     // Generate HTML for filter buttons
+//     const categoryBtns = categories
+//         .map(function (category) {
+//             return `<button type="button" class="filter-btn" data-id=${category}>
+//             ${category}
+//           </button>`;
+//         })
+//         .join("");
+
+//     btnContainer.innerHTML = categoryBtns;
+//     const filterBtns = btnContainer.querySelectorAll(".filter-btn");
+
+//     filterBtns.forEach(function (btn) {
+//         btn.addEventListener("click", function (e) {
+//             const category = e.currentTarget.dataset.id;
+//             const menuCategory = menu.filter(function (menuItem) {
+//                 if (menuItem.category === category) {
+//                     return menuItem;
+//                 }
+//             });
+
+//             // Display menu items based on the selected category
+//             if (category === "all") {
+//                 displayMenuItems(menu);
+//             } else {
+//                 displayMenuItems(menuCategory);
+//             }
+//         });
+//     });
+// }
+
+// // /* async getTemples Function using fetch()*/
+
+// // const getMenu = async () => {
+// //     try {
+// //         const response = await fetch("https://raw.githubusercontent.com/esthercheung728/cse121b/main/images/menu.json");
+// //         if (response.ok) { templeList = await response.json() }
+// //         // menuList = await response.json();
+// //         displayMenuItems(menu);
+// //     } catch (error) {
+// //         console.error("Error fetching temple data:", error);
+// //     }
+// // };
+
+
+// get parent element
+const sectionCenter = document.querySelector(".section-center");
+const btnContainer = document.querySelector(".btn-container");
+
+// display all items when page loads
+window.addEventListener("DOMContentLoaded", function () {
+    fetch("https://esthercheung728.github.io/cse121b/images/menu.json")
+        .then(function (response) {
+            if (!response.ok) {
+                throw new Error("Network response was not ok");
+            }
+            return response.json();
+        })
+        .then(function (data) {
+            displayMenuItems(data);
+            displayMenuButtons(data);
+        })
+        .catch(function (error) {
+            console.log("Error fetching menu data:", error);
+        });
+});
+
+// Function to display menu items
+function displayMenuItems(menuItems) {
+    let displayMenu = menuItems.map(function (item) {
+        return `<article class="menu-item">
+            <img src=${item.img} alt=${item.title} class="photo" />
+            <div class="item-info">
+              <header>
+                <h4>${item.title}</h4>
+                <h4 class="price">$${item.price}</h4>
+              </header>
+              <p class="item-text">
+                ${item.desc}
+              </p>
+            </div>
+          </article>`;
+    });
+
+    displayMenu = displayMenu.join("");
+    sectionCenter.innerHTML = displayMenu;
+}
+
+// Function to display menu filter buttons
+function displayMenuButtons(menuItems) {
+    // Get unique categories from the menu items
+    const categories = menuItems.reduce(
+        function (values, item) {
+            if (!values.includes(item.category)) {
+                values.push(item.category);
+            }
+            return values;
+        },
+        ["all"]
+    );
+
+    // Generate HTML for filter buttons
+    const categoryBtns = categories
+        .map(function (category) {
+            return `<button type="button" class="filter-btn" data-id=${category}>
+            ${category}
+          </button>`;
+        })
+        .join("");
+
+    btnContainer.innerHTML = categoryBtns;
+    const filterBtns = btnContainer.querySelectorAll(".filter-btn");
+
+    filterBtns.forEach(function (btn) {
+        btn.addEventListener("click", function (e) {
+            const category = e.currentTarget.dataset.id;
+            const menuItemsToShow = category === "all" ? menuItems : menuItems.filter(function (item) {
+                return item.category === category;
+            });
+
+            // Display menu items based on the selected category
+            displayMenuItems(menuItemsToShow);
+        });
+    });
+}
